ba3176d495f620dcb2c3abd2e8d2c41164d4d76d
ShadyShawkat <shadyshawkat91@gmail.com>
2021-11-08 15:10:25 +0200

    Fix linters' errors

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
-var navbar = document.querySelector(".navbar");
-var ham = document.querySelector(".ham");
+const navbar = document.querySelector('.navbar');
+const ham = document.querySelector('.ham');
+const menuLinks = document.querySelectorAll('.menuLink');
 
 // toggles hamburger menu in and out when clicking on the hamburger
 function toggleHamburger() {
-  navbar.classList.toggle("showNav");
-  ham.classList.toggle("showClose");
+  navbar.classList.toggle('showNav');
+  ham.classList.toggle('showClose');
 }
 
-ham.addEventListener("click", toggleHamburger);
+ham.addEventListener('click', toggleHamburger);
 
 // toggle when clicking on links
 
 // METHOD 1
-var menuLinks = document.querySelectorAll(".menuLink");
-menuLinks.forEach( (menuLink) => {
-  menuLink.addEventListener("click", toggleHamburger);
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener('click', toggleHamburger);
 });
