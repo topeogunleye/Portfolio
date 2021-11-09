fa495c45aa83dcb3899056b74be37c80f054cf9d
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-11-09 12:50:50 +0100

    Add projects to page dynamically through javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,45 @@
+const PROJECTS = [
+  {
+    name: 'Recipa',
+    description:
+      'Meal Finder App for users to get Meal Recipes Recommendation, Recipe Instructions and Ingredients or Get random recipes.',
+    imageUrl: "'../images/png/Snapshoot-Portfolio1.png'",
+    technologies: ['react', 'tailwind css', 'firebase'],
+    site: 'https://recipa.netlify.app',
+    repo: 'https://github.com/topeogunleye/Recipe-App-V2',
+  },
+
+  {
+    name: 'Portfolio Website',
+    description:
+      'Portfolio website built using Brittany Chiang Portfolio as a template.',
+    imageUrl: "'../images/png/Snapshoot-Portfolio2.png'",
+    technologies: ['html', 'css', 'javascript'],
+    site: 'https://www.topeleye.com/',
+    repo: 'https://github.com/topeogunleye/v4',
+  },
+
+  {
+    name: 'Meal Finder',
+    description:
+      "Meal Finder app I built after following a tutorial by Brad Traversy. I made my version by converting Brad's code from vanilla javascript into react, I added extra functionalities e.g react routing",
+    imageUrl: "'../images/png/Snapshoot-Portfolio2.png'",
+    technologies: ['react', 'tailwind css'],
+    site: 'https://mealdbrecipes.netlify.app/',
+    repo: 'https://github.com/topeogunleye/Recipe-App-V1',
+  },
+
+  {
+    name: 'Portfolio',
+    description:
+      "This website you're looking at right now. It is a project from the first module at Microverse Bootcamp",
+    imageUrl: "'../images/png/Snapshoot-Portfolio2.png'",
+    technologies: ['html', 'css', 'javascript'],
+    site: 'https://github.com/topeogunleye/Recipe-App-V1',
+    repo: 'https://github.com/topeogunleye/Portfolio-Future-Version',
+  },
+];
+
 // https://codepen.io/ljc-dev/embed/GRoLWxj?height=600&default-tab=js,result&embed-version=2
 // https://dev.to/ljcdev/easy-hamburger-menu-with-js-2do0
 const navbar = document.querySelector('.navbar'); // list
@@ -45,3 +87,47 @@ window.onscroll = () => {
     }
   });
 };
+
+window.addEventListener('DOMContentLoaded', () => {
+  PROJECTS.forEach((project) => {
+    const projectDiv = document.createElement('div');
+    projectDiv.className += 'project';
+    projectDiv.className += ' flex';
+
+    projectDiv.innerHTML = `
+    <div class="project-img">
+      <img
+        src=${project.imageUrl}
+        alt="first project"
+      />
+    </div>
+    <div class="project-text">
+      <div class="primary-text">
+        <div class="experience">
+          <h3>${project.name}</h3>
+
+          <ul class="ul">
+            <li class="first-child">Canopy</li>
+            <li>Back End Dev</li>
+            <li>2015</li>
+          </ul>
+        </div>
+      </div>
+      <div class="secondary-text">
+        ${project.description}
+      </div>
+      <div class="skills">
+        <ul>
+        ${project.technologies.map((tech) => `<li>${tech}</li>`).join('')}
+        </ul>
+      </div>
+
+      <button id="open" class="btn btn-skills" type="button">
+        See Project
+      </button>
+    </div>
+    `;
+
+    document.querySelector('.works').appendChild(projectDiv);
+  });
+});
